cf1825f11113f192da66b65d97ec04ec00626ed0
mina-etml <pk60ftv@eduvaud.ch>
2025-12-09 14:39:58 +0100

    feat(book show): separation between parent and child for BookShow and BookInfo

--- passion-lecture/src/components/BookShow/BookInfo.vue
+++ passion-lecture/src/components/BookShow/BookInfo.vue
@@ -1,43 +1,25 @@
 <script setup>
-import {apiGetOneBook} from '@/api/books';
+import { ref} from 'vue'
 import {apiAddAnEval, apiGetOneBookAvgEval} from '@/api/evaluation'
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-const book = ref();
-const bookId = route.params.id;
-const rating = ref([]);
-const userRating = ref([]);
-// const user =  ref(null);
-// const userId =  ref(null);
 
 
-onMounted(async () => {
-  try {
-    const response = await apiGetOneBook(bookId);
-    book.value = response.data; // met le livres dans le ref
+// Parent's props
+const props = defineProps({
+  book: Object,
+  rating: Object
+})
 
-    // userId.value = book.value.userId
-    // const resUser = await apiGetUser(userId.value)
-
-    // // FORMAT BACKEND = [ user, count, lastBook ]
-    // user.value = resUser.data[0]
-
-    const resEval = await apiGetOneBookAvgEval(bookId)
-    rating.value = resEval.data;
-  } catch (error) {
-    console.error("Erreur lors de la récupération du livre :", error);
-  }
-});
+const userRating = ref(null);
 
 const onSubmit = async () => {
 try {
-    await apiAddAnEval(bookId, userRating.value);
+    await apiAddAnEval(props.book.id, userRating.value);
 
     // Refresh avg rating after submitting
-    const resEval = await apiGetOneBookAvgEval(bookId);
-    rating.value = resEval.data;
+    const resEval = await apiGetOneBookAvgEval(props.book.id);
+    props.rating.value = resEval.data;
+    props.rating.average = resEval.data.average
+    props.rating.count = resEval.data.count;
 
   } catch (err) {
     console.error("Erreur lors de l'ajout de la note :", err);
@@ -47,10 +29,6 @@ try {
 
 <template>
     <div class="book"  v-if="book">
-        <router-link :to="{ name: 'userPage', params: { id: book.user.id } }">
-        <h1>Créé par {{ book.user.username }}</h1>
-        </router-link>
-
         <div class="bookimagerating">
 
             <img :src="book.imagePath" alt="book image"></img>
--- passion-lecture/src/views/BookShow.vue
+++ passion-lecture/src/views/BookShow.vue
@@ -1,10 +1,55 @@
 <script setup>
-  import BookInfo from '@/components/BookShow/BookInfo.vue';
+import BookInfo from '@/components/BookShow/BookInfo.vue'
+
+import { apiGetOneBook } from '@/api/books'
+import { apiGetOneBookAvgEval } from '@/api/evaluation'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const book = ref()
+const bookId = route.params.id
+const rating = ref([]);
+
+// const user =  ref(null);
+// const userId =  ref(null);
+
+onMounted(async () => {
+  try {
+    const response = await apiGetOneBook(bookId)
+    book.value = response.data // met le livres dans le ref
+
+    // userId.value = book.value.userId
+    // const resUser = await apiGetUser(userId.value)
+
+    // // FORMAT BACKEND = [ user, count, lastBook ]
+    // user.value = resUser.data[0]
+
+    const resEval = await apiGetOneBookAvgEval(bookId)
+    rating.value = resEval.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération du livre :', error)
+  }
+})
 </script>
 
 <template>
-    <BookInfo></BookInfo>
+  <div v-if="book && rating">
+  <router-link :to="{ name: 'userPage', params: { id: book.user.id } }">
+    <h1>Créé par {{ book.user.username }}</h1>
+  </router-link>
+
+  <BookInfo :book="book" :rating="rating" />
+  </div>
 </template>
 
 <style scoped>
-</style>
+h1{
+    text-align: center;
+}
+h2{
+    border-bottom: 1px solid;
+}
+a {
+    text-decoration: none;
+}</style>
