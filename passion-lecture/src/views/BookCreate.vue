8f934c7f543d8117fec08f3c5b78f1dc54594ae5
Brendan Fleurdelys <pl76tup@eduvaud.ch>
2025-12-17 08:30:33 +0100

    feat: can add new book

--- passion-lecture/src/views/BookCreate.vue
+++ passion-lecture/src/views/BookCreate.vue
@@ -1,7 +1,61 @@
 <script setup>
 import BookForm from '@/components/BookCreateUpdate/BookForm.vue';
+import router from '@/router';
+import { apiAddBook } from '@/api/books';
+import { ref, onMounted } from 'vue';
+import { apiGetAllAuthors } from '@/api/category';
+import { apiGetAllCategories } from '@/api/category';
+
+
+const bookData = ref(null);
+const authorsList = ref([]);
+const categoriesList = ref([]);
+const isLoading = ref(true);
+
+onMounted(async () => {
+  try {
+    const [authorsResponse, categoriesResponse] = await Promise.all([
+      apiGetAllAuthors(),
+      apiGetAllCategories(),
+    ])
+    bookData.value = {
+      title: '',
+      authorId: '',
+      categoryId: '',
+      numberOfPages: '',
+      editor: '',
+      editionYear: '',
+      abstract: '',
+      imagePath: '',
+      pdfLink: '',
+    };
+    authorsList.value = authorsResponse.data;
+    categoriesList.value = categoriesResponse.data;
+
+  } catch (e) {
+    console.error("Erreur de chargement", e);
+  } finally {
+    isLoading.value = false;
+  }
+});
+
+const handleSave = async (dataToSave) => {
+  try {
+    await apiAddBook(dataToSave);
+
+    router.push('/mybooks');
+  } catch (error) {
+    console.error("Erreur lors de la création du livre", error);
+  }
+};
 </script>
+
 <template>
-  <BookForm></BookForm>
+  <div class="container mx-auto p-4">
+    <div v-if="isLoading" class="text-center">Chargement...</div>
+
+    <BookForm v-else :initial-data="bookData" :is-editing="false" :authors="authorsList" :categories="categoriesList"
+      @submit-form="handleSave" @cancel="router.back()" />
+  </div>
 </template>
 <style></style>
